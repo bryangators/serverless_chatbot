0088178133fd38b8ee03bf8e4a53080f31065c90
Bryan Kristofferson <bryangators510@gmail.com>
2025-02-16 21:54:00 -0500

    formatted tf file

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1" 
+  region = "us-east-1"
 }
 
 resource "aws_iam_role" "lambda_role" {
-name   = "serverless_chatbot_role"
-assume_role_policy = <<EOF
+  name               = "serverless_chatbot_role"
+  assume_role_policy = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -22,11 +22,11 @@ EOF
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
- 
- name         = "aws_iam_policy_for_terraform_aws_lambda_role"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
+
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -45,26 +45,26 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
- role        = aws_iam_role.lambda_role.name
- policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
 data "archive_file" "zip_the_python_code" {
-type        = "zip"
-source_dir  = "${path.module}/python/"
-output_path = "${path.module}/python/hello-python.zip"
+  type        = "zip"
+  source_dir  = "${path.module}/python/"
+  output_path = "${path.module}/python/hello-python.zip"
 }
 
 resource "aws_lambda_function" "terraform_lambda_func" {
-filename                       = "${path.module}/python/hello-python.zip"
-function_name                  = "Serverless_Chatbot_Lambda_Function"
-role                           = aws_iam_role.lambda_role.arn
-handler                        = "index.lambda_handler"
-runtime                        = "python3.8"
-depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  filename      = "${path.module}/python/hello-python.zip"
+  function_name = "Serverless_Chatbot_Lambda_Function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
 resource "aws_lambda_function_url" "lambda_url" {
   function_name      = aws_lambda_function.my_lambda.function_name
-  authorization_type = "NONE" 
+  authorization_type = "NONE"
 }
